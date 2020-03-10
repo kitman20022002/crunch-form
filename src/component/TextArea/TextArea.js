2830fb7d6bc7725a8c92ceb332f8694ce982c46a
kitman20022002 <kitman200220022002@gmail.com>
2020-03-11 08:12:02 +1100

    Fix textarea

--- src/component/TextArea/TextArea.js
+++ src/component/TextArea/TextArea.js
@@ -10,14 +10,16 @@ class TextArea extends Component {
             <React.Fragment>
                 <div className={"container-text-area"}>
                     <label htmlFor={name}>{!this.props.disabled ? label : label.replace(' (optional)', '')}</label>
+                    <div className={"container-error"}>
                     <textarea name={name}
                               value={value}
                               onChange={onChange}
                               className={error && !error.isValid ? "error" : ''}
                               {...this.props}
                     />
-                    {error && !error.isValid &&
-                    <MdErrorOutline datatest-id={"error"} className={"icon-error"} color={"#FF516D"}/>}
+                        {error && !error.isValid &&
+                        <MdErrorOutline datatest-id={"error"} className={"icon-error"} color={"#FF516D"}/>}
+                    </div>
                 </div>
             </React.Fragment>
         )
